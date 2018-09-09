4caec9da3601537dcd4f30ae53847b6417e0c130
Gian Sass <gian.sass@outlook.de>
2018-09-09 18:07:20 +0200

    Can load token from config file now

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const fs = require('fs');
 const Bot = require('./lib/bot');
 
-const token = process.env.TOKEN;
+const loadTokenFromConfig = () => {
+  try {
+    let file = fs.readFileSync('config.json', 'utf8');
+    return JSON.parse(file).token;
+  } catch(err) {
+    console.error('Error reading config.json: ' + err);
+    process.exit(1);
+  }
+}
+
+const token = process.env.TOKEN || loadTokenFromConfig();
 
 if (!token) {
   console.error('Specify a token using the TOKEN environment variable');
